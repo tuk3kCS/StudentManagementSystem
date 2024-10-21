676c3c3e821c462d1a014c6f424bdf71eb8b91fc
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-10-22 01:45:34 +0700

    Update User.java

--- src/stu_mng_sys/User.java
+++ src/stu_mng_sys/User.java
@@ -1,5 +1,5 @@
 
-package Student_Management_System;
+package stu_mng_sys;
 import java.util.*; 
 public class User {
     Scanner sc = new Scanner(System.in); 
@@ -23,28 +23,12 @@ public class User {
     public String getUsername(){
         return this.UserName;
     }
-    
-    public void setRole(String Role){
-        this.Role = Role; 
-    }
-    
-    public String getRole(){
-        return this.Role;
-    }
-    
+     
     public void getID(){
         String ID = sc.nextLine();
         this.ID = ID; 
     }
-    
-    public void setPassword(String Password){
-        this.Password = Password; 
-    }
-    
-    public String getPassword(){
-        return this.Password; 
-    }
-    
+     
     public String toString(){
         return this.ID + " " + this.UserName + " " + this.Password + " " + this.Role; 
     }
