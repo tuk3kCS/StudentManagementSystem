87c3429f82b0e7d45556ace8862561ac624ec6b4
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-22 12:52:37 +0700

    Update Classes.java

--- src/stu_mng_sys/Classes.java
+++ src/stu_mng_sys/Classes.java
@@ -1,47 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package stu_mng_sys;
 import java.util.*;
 public class Classes {
     private String classID, Major;
+    private ArrayList<Student> students; //Composition: A class has many students. 
     
-    Classes(){
-        
-    }
-    
-    Classes(String classID, String Major){
+    Classes(String classID, String Major,ArrayList<Student> student){
         this.classID = classID;
         this.Major = Major;
+        this.students = student;
     }
-    
-    public void setClassID(String s){
-        this.classID = s; 
-    }
-    
-    public String getClassID(){
-        return this.classID; 
-    }
-    
-    public void setMajor(String s){
-        this.Major = s; 
-    }
-    
-    public String getMajor(){
-        return this.Major; 
-    }
-    
+
     public void addNewClass(){
         
     }
     
     public void viewClassInfor(){
         
-    }
-    
-    public String toString(){
-        return this.classID + " " + this.Major;  
-    }
-    
+    }  
 }
