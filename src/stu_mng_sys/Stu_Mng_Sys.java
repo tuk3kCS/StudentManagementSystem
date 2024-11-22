72f2968c3e3f4324898e2345639a819c05d83c0d
tuk3kCS <tungnh0308@gmail.com>
2024-11-22 17:50:50 +0700

    Make minor changes that project can be run on both NetBeans and IntelliJ

--- src/stu_mng_sys/Stu_Mng_Sys.java
+++ src/stu_mng_sys/Stu_Mng_Sys.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package stu_mng_sys;
 
-/**
- *
- * @author dinhq
- */
 public class Stu_Mng_Sys {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
