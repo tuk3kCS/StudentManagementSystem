1e53c09f3cdfe1f5f2d3dc8635583defd2d728a1
tuk3kCS <tungnh0308@gmail.com>
2024-11-23 22:21:47 +0700

    Create scoresWindow.java

---
+++ src/stu_mng_sys/scoresWindow.java
@@ -0,0 +1,21 @@
+package stu_mng_sys;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class scoresWindow extends JFrame {
+    private final Stu_Mng_Sys mainApp;
+
+    public scoresWindow(Stu_Mng_Sys mainApp) {
+        //Create student management function window
+        super("Student Management");
+        this.mainApp = mainApp;
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setSize(1280, 720);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+}
