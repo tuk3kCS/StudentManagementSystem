533d459d70b2eb2e1e36f4d8ac9fb48909e94f5f
tuk3kCS <tungnh0308@gmail.com>
2024-11-23 22:22:46 +0700

    Rebuild homepage

--- src/stu_mng_sys/Stu_Mng_Sys.java
+++ src/stu_mng_sys/Stu_Mng_Sys.java
@@ -5,116 +5,86 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
-public class Stu_Mng_Sys {
+public class Stu_Mng_Sys extends JFrame {
+    private final studentWindow stWindow;
+    private final classesWindow clWindow;
+    private final subjectWindow suWindow;
+    private final scoresWindow scWindow;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(Stu_Mng_Sys::new); //Make this program thread-safe (?)
     }
 
     public Stu_Mng_Sys() {
-        //Create main frame
+        //Create windows for all functions
+        stWindow = new studentWindow(this);
+        clWindow = new classesWindow(this);
+        suWindow = new subjectWindow(this);
+        scWindow = new scoresWindow(this);
+
+        //Create main program frame
         JFrame mainFrame = new JFrame("Student Management System");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(1280, 720);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setResizable(false);
         mainFrame.setVisible(true);
-        mainFrame.setLayout(new BorderLayout());
+        mainFrame.setLayout(null);
 
-        //Create function panel
-        JPanel studentPanel = new JPanel();
-        JPanel classesPanel = new JPanel();
-        JPanel subjectPanel = new JPanel();
-        JPanel scoresPanel = new JPanel();
+        //Create homepage
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null);
+        mainPanel.setBounds(0, 0, 1280, 720);
+        mainFrame.add(mainPanel);
 
-        //Create menubar panel with 4 buttons correspond to 4 functions
-        JPanel menuBar = new JPanel();
-        menuBar.setLayout(new GridLayout(4, 1));
-        menuBar.setPreferredSize(new Dimension(320, 720));
-        mainFrame.add(menuBar, BorderLayout.WEST);
+        //Create contents in homepage
+        JLabel mainLabel = new JLabel("Student Management System"); //homepage label
+        mainLabel.setFont(new Font("Arial", Font.BOLD, 60));
+        mainLabel.setBounds(230, 80, 1000, 100);
+        mainPanel.add(mainLabel);
 
         JButton studentButton = new JButton("Student Management"); //Student management button
+        studentButton.setFont(new Font("Arial", Font.BOLD, 25));
+        studentButton.setBounds(160, 280, 400, 100);
         studentButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                studentPanel.setVisible(true);
-                classesPanel.setVisible(false);
-                subjectPanel.setVisible(false);
-                scoresPanel.setVisible(false);
+                stWindow.setVisible(true); //Student management window visible when pressing the button
             }
         });
-        menuBar.add(studentButton);
+        mainPanel.add(studentButton);
 
         JButton classesButton = new JButton("Classes Management"); //Classes management button
+        classesButton.setFont(new Font("Arial", Font.BOLD, 25));
+        classesButton.setBounds(720, 280, 400, 100);
         classesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                studentPanel.setVisible(false);
-                classesPanel.setVisible(true);
-                subjectPanel.setVisible(false);
-                scoresPanel.setVisible(false);
+                clWindow.setVisible(true); //Classes management window visible when pressing the button
             }
         });
-        menuBar.add(classesButton);
+        mainPanel.add(classesButton);
 
         JButton subjectButton = new JButton("Subject Management"); //Subject management button
+        subjectButton.setFont(new Font("Arial", Font.BOLD, 25));
+        subjectButton.setBounds(160, 450, 400, 100);
         subjectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                studentPanel.setVisible(false);
-                classesPanel.setVisible(false);
-                subjectPanel.setVisible(true);
-                scoresPanel.setVisible(false);
+                suWindow.setVisible(true); //Subject management window visible when pressing the button
             }
         });
-        menuBar.add(subjectButton);
+        mainPanel.add(subjectButton);
 
         JButton scoresButton = new JButton("Scores Management"); //Scores management button
+        scoresButton.setFont(new Font("Arial", Font.BOLD, 25));
+        scoresButton.setBounds(720, 450, 400, 100);
         scoresButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                studentPanel.setVisible(false);
-                classesPanel.setVisible(false);
-                subjectPanel.setVisible(false);
-                scoresPanel.setVisible(true);
+                scWindow.setVisible(true); //Scores management window visible when pressing the button
             }
         });
-        menuBar.add(scoresButton);
-
-        //Create functions in student management panel
-        studentPanel.setLayout(null);
-        mainFrame.add(studentPanel, BorderLayout.CENTER);
-
-        JLabel studentLabel = new JLabel("Student Management"); //Student Management label
-        studentLabel.setFont(new Font("Arial", Font.BOLD, 40));
-        studentLabel.setBounds(280, 50, 500, 60);
-        studentPanel.add(studentLabel);
-
-        JButton addStudentButton = new JButton("Add New Student"); // Add new student function button
-        addStudentButton.setBounds(50, 150, 200, 30);
-        addStudentButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        studentPanel.add(addStudentButton);
-
-        String[][] studentInit = {};
-        String[] studentAttributes = {"Student ID", "Full Name", "Date of Birth", "Class", "Major"};
-        JTable studentTable = new JTable(studentInit, studentAttributes);
-        JScrollPane studentScrollPane = new JScrollPane(studentTable); //Create students' information table
-        studentPanel.add(studentScrollPane);
-        studentScrollPane.setBounds(40, 200, 860, 420);
-
-        //Create functions in classes management
-        classesPanel.setLayout(null);
-        mainFrame.add(classesPanel, BorderLayout.CENTER);
-
-        JLabel classesLabel = new JLabel("Classes Management"); //Classes Management label
-        classesLabel.setFont(new Font("Arial", Font.BOLD, 40));
-        classesLabel.setBounds(280, 50, 500, 60);
-        classesPanel.add(classesLabel);
+        mainPanel.add(scoresButton);
     }
-
 }
