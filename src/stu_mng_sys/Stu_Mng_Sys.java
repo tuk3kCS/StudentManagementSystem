e22fca720056123f2fb62673a85a89f95014a467
Tung Nguyen Hoang <80618091+tuk3kCS@users.noreply.github.com>
2024-11-25 00:01:43 +0700

    Revert "Merge main to marcusd"

--- src/stu_mng_sys/Stu_Mng_Sys.java
+++ src/stu_mng_sys/Stu_Mng_Sys.java
@@ -1,9 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package stu_mng_sys;
 
+/**
+ *
+ * @author dinhq
+ */
 public class Stu_Mng_Sys {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // TODO code application logic here
     }
     
 }
