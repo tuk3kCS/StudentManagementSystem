1a94c1cd5aac63de0d0dd0e4954f9db8001b3ef5
Tung Nguyen Hoang <tungnh0308@gmail.com>
2024-12-02 17:12:26 +0700

    Update studentWindow.java

--- src/stu_mng_sys/studentWindow.java
+++ src/stu_mng_sys/studentWindow.java
@@ -7,7 +7,7 @@ import java.util.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
-public class studentWindow extends JFrame implements Serializable {
+public class studentWindow extends JFrame {
     private final JLabel studentLabel = new JLabel("Student Management");
     private final JButton addStudentButton = new JButton("Add New Student");
     private final JButton modifyInfoButton = new JButton("Modify Information");
